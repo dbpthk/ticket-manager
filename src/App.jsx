ecfe9d80ceab08ece088f11cfa0db981baf7bf44
Dhruv <dbpthk@gmail.com>
2026-01-13 13:21:21 +1100

    ticket manager first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Ticket Management Application</h1>
+        <p>Manage and track customer tickets efficiently</p>
+      </header>
+    </div>
+  );
+}
